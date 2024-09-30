661d11965afa1ff7da50fa704a39cbecb178f366
Sarvesh Sekar <sarveshcs2004@gmail.com>
2024-09-30 07:21:33 +0530

    Create app.vue

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <ImageGallery />
+  </div>
+</template>
+
+<script>
+import ImageGallery from './components/ImageGallery.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ImageGallery,
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap');
+
+body {
+  font-family: 'Roboto', sans-serif;
+}
+
+#app {
+  text-align: center;
+}
+</style>
